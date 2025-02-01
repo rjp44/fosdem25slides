28c3223059f891377ca2e6974c9d51e8da62fff9
Rob Pickering <rob@pickering.org>
2025-02-01 12:22:26 +0000

    Fix get_next_slide context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,98 @@
 'use client'
 import AplisayWidget from '@aplisay/react-widget';
 import '@aplisay/react-widget/dist/styles.css';
-import { useState, useEffect } from 'react';
-import  getSlides from '../lib/dl.js';
-import { useDynamicRouteParams } from 'next/dist/server/app-render/dynamic-rendering.js';
+import { useState, useEffect, useCallback } from 'react';
+import getSlides from '../lib/dl.js';
+
 const url = process.env.NEXT_PUBLIC_APLISAY_URL;
 const roomKey = process.env.NEXT_PUBLIC_APLISAY_KEY;
 const listenerId = process.env.NEXT_PUBLIC_APLISAY_AGENT;
-
-
-
 
 const ImageSlider: React.FC = () => {
   const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
   const [open, setOpen] = useState(false);
   const [slides, setSlides] = useState([]);
+  const [numSlides, setNumSlides] = useState(0)
+
+
 
   useEffect(() => {
-    getSlides().then(setSlides);
+    function keyDown(event) {
+      console.log(event.key, 'key');
+      if (event.key === 'ArrowRight' || event.key === 'PageDown' || event.key === 'Enter') {
+        nextSlide();
+      } else if (event.key === 'ArrowLeft' || event.key === 'PageUp') {
+        lastSlide();
+      }
+    }
+
+    document.addEventListener('keydown', keyDown);
+    return () =>
+      document.removeEventListener('keydown', keyDown);
+  }, [numSlides]);
+
+
+  useEffect(() => {
+    getSlides().then((s) => {
+      setSlides(s);
+      setNumSlides(s?.length);
+      console.log('num slides', s?.length);
+    });
+
   }, []);
 
-  useEffect( () => {
+  useEffect(() => {
     console.log({ slides, currentSlideIndex }, 'slides');
-  }, [slides, currentSlideIndex]);
+  }, [slides, currentSlideIndex, numSlides]);
 
-
-  const get_current_slide = (params) => {
-    console.log({ params }, 'currentSlide');
+  let current_slide;
+  
+  const get_current_slide = () => {
+    current_slide = currentSlideIndex;
     return JSON.stringify(slides[currentSlideIndex]);
   };
 
-  const get_next_slide = (params) => {
-    const nextSlide = (currentSlideIndex + 1) % slides.length;
-    console.log({ params }, 'nextSlide');
-    nextImage();
-    return JSON.stringify(slides[nextSlide]);
-  };
 
-  const lastImage = () => {
+
+  const lastSlide = () => {
     setCurrentSlideIndex((prevIndex) => Math.max((prevIndex - 1), 0));
   };
 
 
-  const nextImage = () => {
-    setCurrentSlideIndex((prevIndex) => Math.min((prevIndex + 1), slides.length - 1));
+  function nextSlide() {
+    setCurrentSlideIndex((prevIndex) => {
+      const newIndex = Math.min((prevIndex + 1), numSlides - 1)
+      console.log({ prevIndex, newIndex, length: numSlides });
+      return newIndex;
+    });
+    return Math.min((currentSlideIndex + 1), numSlides - 1)
   };
+
+  const get_next_slide = useCallback(() => {
+    current_slide = Math.min((current_slide + 1), numSlides - 1);
+    setCurrentSlideIndex(current_slide);
+    console.log({ current_slide, numSlides }, 'next slide');
+    return JSON.stringify(slides[current_slide]);
+  }, [currentSlideIndex, numSlides, slides, nextSlide]);
 
   return (
     <>
 
-      <img src={slides[currentSlideIndex]?.image} alt="Slider image"
+      <img src={slides[currentSlideIndex]?.Iimage} alt="Slider image"
         style={{
           width: '100%',
-          height: '90vh',
+          height: '100vh',
           objectFit: 'cover'
         }}
       />
-        <div style={{
-          width: '20vw',
-          position: 'absolute',
-          top: '10px',
-          right: '10px',
-          height: '100vh',
-          backgroundColor: 'transparent'
-       }}>
+      <div style={{
+        width: '20vw',
+        position: 'absolute',
+        top: '10px',
+        right: '10px',
+        height: '100vh',
+        backgroundColor: 'transparent'
+      }}>
         <AplisayWidget
           url={url}
           listenerId={listenerId}
@@ -87,12 +116,13 @@ const ImageSlider: React.FC = () => {
       </div>
 
       <div style={{
-        display: 'flex',
-        justifyContent: 'center'
+        position: 'absolute',
+        bottom: '10px',
+        left: '10px',
+        backgroundColor: 'transparent'
       }}>
-      <button disabled={currentSlideIndex === 0} onClick={lastImage}>Last</button>
-      <button disabled={currentSlideIndex === slides.length - 1} onClick={nextImage}>Next</button>
-        <button onClick={() => setOpen(true)}>Agent</button>
+        {currentSlideIndex +1}/{numSlides}
+        <button onClick={() => setOpen(true)}>Hand over to Emily</button>
       </div>
     </>
   );
