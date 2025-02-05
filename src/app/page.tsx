88b7b77a187a2ceef35aa0cf467ead550db28d93
Rob Pickering <rob@pickering.org>
2025-02-05 17:19:04 +0000

    Cleaner client tools calling logic, add useRef

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,87 +2,120 @@
 import { useSwipeable } from 'react-swipeable';
 import AplisayWidget from '@aplisay/react-widget';
 import '@aplisay/react-widget/dist/styles.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { SlideType } from '../types';
 import staticSlides from '../slides';
+
 const url = process.env.NEXT_PUBLIC_APLISAY_URL;
 const roomKey = process.env.NEXT_PUBLIC_APLISAY_KEY;
 const listenerId = process.env.NEXT_PUBLIC_APLISAY_AGENT;
 
-const ImageSlider: React.FC = () => {
-  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
-  const [open, setOpen] = useState(false);
+interface CallbacksType {
+  [key: string]: () => string | null
+};
+
+function useSlides() {
   const [slides, setSlides] = useState<SlideType[] | null>(null);
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
   const [numSlides, setNumSlides] = useState(0);
+
+  // We need to set and use Refs for current slide and number of slides
+  //  as the get_current_slide and get_next_slide callbacks that get passed as
+  //  dynamic client tools to the LLM Widget are frozen at the time the widget is first
+  //  rendered `open`. By dereferencing the current values of the Ref, we get the current
+  //  state of the visual component to give to the LLM callback. There are probably other
+  //  ways to do this. None of them seem to be clean.
+  const currentSlideRef = useRef(0);
+  const numSlidesRef = useRef(0);
+  currentSlideRef.current = currentSlideIndex;
+  numSlidesRef.current = numSlides;
+ 
+  useEffect(() => {
+    setSlides(staticSlides);
+    setNumSlides(staticSlides?.length || 0)
+  }, []);
+
+  const get_current_slide = (): string | null => {
+    // Use the ref.
+    const index = currentSlideRef?.current || 0;
+    console.log({ currentSlideRef, slide: slides?.[index] });
+    return slides && JSON.stringify(slides?.[currentSlideIndex]);
+  };
+
+  const get_next_slide = (): string | null => {
+    const index = currentSlideRef?.current || 0;
+    const max = numSlidesRef?.current || 0;
+    const nextSlide = Math.min((index + 1), max - 1);
+    console.log({ nextSlide, slide: slides?.[nextSlide] });
+    setCurrentSlideIndex(nextSlide);
+    return slides && JSON.stringify(slides?.[nextSlide]);
+  };
+
+
+  const callbacks: CallbacksType = {
+    get_current_slide,
+    get_next_slide
+  };
+
+
+
+  const advanceSlide = () =>
+    setCurrentSlideIndex((prevIndex) => Math.min((prevIndex + 1), numSlides - 1));
+
+  const retardSlide = () =>
+    setCurrentSlideIndex((prevIndex) => Math.max((prevIndex - 1), 0));
+
+  return { slides, currentSlideIndex, numSlides, callbacks, advanceSlide, retardSlide };
+}
+
+const ImageSlider: React.FC = () => {
+  const { slides,
+    currentSlideIndex,
+    numSlides,
+    advanceSlide,
+    retardSlide,
+    callbacks
+  } = useSlides();
+  const [open, setOpen] = useState(false);
   const [fullScreen, setFullScreen] = useState(false);
 
   const handlers = useSwipeable({
-    onSwipedLeft: nextSlide,
-    onSwipedRight: lastSlide,
+    onSwipedLeft: advanceSlide,
+    onSwipedRight: retardSlide,
     onTap: () => {
       console.log('tap');
-      nextSlide();
+      advanceSlide();
     },
     trackMouse: true,
   });
 
   useEffect(() => {
-    function keyDown(event: { key: string }) {
+    const keyDown = (event: KeyboardEvent) => {
       console.log(event.key, 'key');
       if (event.key === 'ArrowRight' || event.key === 'PageDown' || event.key === 'Enter') {
-        nextSlide();
+        advanceSlide();
       } else if (event.key === 'ArrowLeft' || event.key === 'PageUp') {
-        lastSlide();
+        retardSlide();
       }
     }
     document.addEventListener('keydown', keyDown);
     return () =>
       document.removeEventListener('keydown', keyDown);
-  }, [numSlides, nextSlide, lastSlide]);
+  }, [advanceSlide, retardSlide]);
 
 
-  useEffect(() => {
-    setSlides(staticSlides);
-    setNumSlides(staticSlides.length);
-    console.log({ slides });
-  }, []);
 
   useEffect(() => {
     console.log({ slides, currentSlideIndex }, 'slides');
-  }, [slides, currentSlideIndex, numSlides]);
+  }, [slides, currentSlideIndex]);
 
-  let current_slide = 0;
-
-  const get_current_slide = () => {
-    current_slide = currentSlideIndex;
-    return slides && JSON.stringify(slides[currentSlideIndex]);
-  };
-
-  function lastSlide() {
-    setCurrentSlideIndex((prevIndex) => Math.max((prevIndex - 1), 0));
-  };
-
-
-  function nextSlide() {
-    setCurrentSlideIndex((prevIndex) => {
-      const newIndex = Math.min((prevIndex + 1), numSlides - 1)
-      console.log({ prevIndex, newIndex, length: numSlides });
-      return newIndex;
-    });
-  };
-  const get_next_slide = () => {
-    current_slide = Math.min((current_slide + 1), numSlides - 1);
-    setCurrentSlideIndex(current_slide);
-    console.log({ current_slide, numSlides }, 'next slide');
-    return JSON.stringify(slides?.[current_slide]);
-  };
 
   useEffect(() => {
     if (fullScreen) {
       document.documentElement.requestFullscreen();
     } else {
-      if(document.fullscreenElement !== null)
-       document?.exitFullscreen();
+      if (document.fullscreenElement !== null)
+        document?.exitFullscreen();
     }
   }, [fullScreen]);
 
@@ -113,11 +146,8 @@ const ImageSlider: React.FC = () => {
           url={url}
           listenerId={listenerId}
           roomKey={roomKey}
-          clientFunctions={{
-            get_current_slide,
-            get_next_slide
-          }}
-          debug={true}
+          clientFunctions={callbacks}
+          debug={false}
           showStatus={true}
           open={open}
           theme={{
@@ -167,19 +197,18 @@ const ImageSlider: React.FC = () => {
           cursor: 'pointer'
         }}
       >
-        {!fullScreen ?
-          <svg width="25px" height="25px" viewBox="0 0 24 24" fill="#dddddd" xmlns="http://www.w3.org/2000/svg">
+        {!fullScreen
+          ? <svg width="25px" height="25px" viewBox="0 0 24 24" fill="#dddddd" xmlns="http://www.w3.org/2000/svg">
             <path
               fillRule="evenodd"
               clipRule="evenodd"
               d="M18 4.654v.291a10 10 0 0 0-1.763 1.404l-2.944 2.944a1 1 0 0 0 1.414 1.414l2.933-2.932A9.995 9.995 0 0 0 19.05 6h.296l-.09.39A9.998 9.998 0 0 0 19 8.64v.857a1 1 0 1 0 2 0V4.503a1.5 1.5 0 0 0-1.5-1.5L14.5 3a1 1 0 1 0 0 2h.861a10 10 0 0 0 2.249-.256l.39-.09zM4.95 18a10 10 0 0 1 1.41-1.775l2.933-2.932a1 1 0 0 1 1.414 1.414l-2.944 2.944A9.998 9.998 0 0 1 6 19.055v.291l.39-.09A9.998 9.998 0 0 1 8.64 19H9.5a1 1 0 1 1 0 2l-5-.003a1.5 1.5 0 0 1-1.5-1.5V14.5a1 1 0 1 1 2 0v.861a10 10 0 0 1-.256 2.249l-.09.39h.295z" />
           </svg>
-          :
-          <svg width="25px" height="25px" viewBox="0 0 24 24" fill="#dddddd" xmlns="http://www.w3.org/2000/svg">
+          : <svg width="25px" height="25px" viewBox="0 0 24 24" fill="#dddddd" xmlns="http://www.w3.org/2000/svg">
             <path d="M21.7071 3.70711L16.4142 9H20C20.5523 9 21 9.44772 21 10C21 10.5523 20.5523 11 20 11H14.0007L13.997 11C13.743 10.9992 13.4892 10.9023 13.295 10.7092L13.2908 10.705C13.196 10.6096 13.1243 10.4999 13.0759 10.3828C13.0273 10.2657 13.0004 10.1375 13 10.003L13 10V4C13 3.44772 13.4477 3 14 3C14.5523 3 15 3.44772 15 4V7.58579L20.2929 2.29289C20.6834 1.90237 21.3166 1.90237 21.7071 2.29289C22.0976 2.68342 22.0976 3.31658 21.7071 3.70711Z"
-              />
+            />
             <path d="M9 20C9 20.5523 9.44772 21 10 21C10.5523 21 11 20.5523 11 20V14.0007C11 13.9997 11 13.998 11 13.997C10.9992 13.7231 10.8883 13.4752 10.7092 13.295C10.7078 13.2936 10.7064 13.2922 10.705 13.2908C10.6096 13.196 10.4999 13.1243 10.3828 13.0759C10.2657 13.0273 10.1375 13.0004 10.003 13C10.002 13 10.001 13 10 13H4C3.44772 13 3 13.4477 3 14C3 14.5523 3.44772 15 4 15H7.58579L2.29289 20.2929C1.90237 20.6834 1.90237 21.3166 2.29289 21.7071C2.68342 22.0976 3.31658 22.0976 3.70711 21.7071L9 16.4142V20Z"
-             />
+            />
           </svg>
         }
 
